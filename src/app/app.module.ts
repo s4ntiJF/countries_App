db05e4186ef626f014e7e7e631f7adfcaeeecfb9
Santiago Jimenez <sjjf1993@gmail.com>
2023-11-13 12:31:56 +0100

    The application of search countries by capital, by countrie and by region is finished

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/components/loading-skinner/loading-skinner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-loading-skinner',
+  templateUrl: './loading-skinner.component.html',
+  styleUrls: ['./loading-skinner.component.css']
+})
+export class LoadingSkinnerComponent {
+
+}
